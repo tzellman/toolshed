367bdf552ea4cd12cc182e78fdad26d0e8ddd177
Tom Zellman <tzellman@gmail.com>
2007-12-10 15:01:57 +0000

    updated the app in the following way: - accepts --default and --window arguments - uses a default madlib file, which resides local to the class - doesn't output the madlib to an html file, but uses the text as-is

--- galoot/src/java/galoot/examples/madlibs/MadlibExample.java
+++ galoot/src/java/galoot/examples/madlibs/MadlibExample.java
@@ -12,19 +12,23 @@ import galoot.Template;
 import java.awt.Dimension;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.XMLConfiguration;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -40,6 +44,17 @@ public class MadlibExample
     // **** The configuration file that defines our vocabulary
     private static final String MADLIB_CONFIG = "madlib.config";
 
+    private static final String DEFAULT_MADLIB = "FullMadLib.galoot";
+
+    private static void showHelp(String err)
+    {
+        if (err != null)
+            System.out.println("ERROR: " + err);
+        System.out.println("Usage: " + MadlibExample.class
+                + " [--default] [--window] <madlib file>");
+        System.exit(0);
+    }
+
     /**
      * Sample illustrating the features of the galoot framework
      * 
@@ -51,19 +66,60 @@ public class MadlibExample
     public static void main(String[] args) throws ConfigurationException,
             FileNotFoundException, IOException
     {
-        if (!(args.length == 1 || args.length == 2))
+        boolean useWindow = false;
+        String filename = null;
+        for (int i = 0; i < args.length; i++)
         {
-            System.out.println("Usage: " + MadlibExample.class + " "
-                    + "<madlib file> <show in browser>");
-            System.exit(0);
+            String arg = args[i];
+            if (arg.equals("--default"))
+            {
+                // use the default one
+                filename = DEFAULT_MADLIB;
+            }
+            else if (arg.equals("--window"))
+            {
+                useWindow = true;
+            }
+            else
+                filename = arg;
         }
 
-        File file = new File(args[0]);
+        if (filename == null)
+            showHelp(null);
+
+        // try to get the file as a file...
+        File file = new File(filename);
+        InputStreamReader streamReader = null;
+        if (!file.exists() || !file.isFile())
+        {
+            // try to load it relative to this class location
+            URL resource = MadlibExample.class.getResource(filename);
+            if (resource == null)
+                showHelp("Unable to load default MadLib -- try using your own");
+            streamReader = new InputStreamReader(resource.openStream());
+        }
+        else
+        {
+            // it is a valid file
+            streamReader = new FileReader(file);
+
+            // register the directory where our templates live
+            PluginRegistry.getInstance().addTemplateIncludePath(
+                    file.getParent());
+        }
+
+        if (streamReader == null)
+            showHelp(null);
+
+        StringWriter stringWriter = new StringWriter();
+        BufferedReader reader = new BufferedReader(streamReader);
+        char buf[] = new char[1024];
+        int numRead;
+        while ((numRead = reader.read(buf)) > 0)
+            stringWriter.write(buf, 0, numRead);
 
         // register a filter with the plugin registry
         PluginRegistry.getInstance().registerFilter(new Underline());
-        // register the directory where our templates live
-        PluginRegistry.getInstance().addTemplateIncludePath(file.getParent());
 
         // create a new input adapter that generates data for feeding to a
         // template
@@ -75,32 +131,32 @@ public class MadlibExample
         ContextStack contextStack = new ContextStack(madlibInputAdapter
                 .getContextStackInput());
 
-        Template template = new Template(new File(args[0]));
+        Template template = new Template(stringWriter.toString());
         String filledInMadlibs = template.render(contextStack);
 
         // dump the output of the replace
         System.out.println(filledInMadlibs);
-        File madlibFile = new File("madlib.html");
+        // File madlibFile = new File("madlib.html");
 
-        FileUtils.writeStringToFile(madlibFile, filledInMadlibs, "UTF-8");
+        // FileUtils.writeStringToFile(madlibFile, filledInMadlibs, "UTF-8");
 
         // display in a mini web browser
         if (args.length == 2)
         {
             try
             {
-                URL[] urls = FileUtils.toURLs(new File[] { madlibFile });
+                // URL[] urls = FileUtils.toURLs(new File[] { madlibFile });
                 JFrame frame = new JFrame();
 
                 Dimension screenSize = Toolkit.getDefaultToolkit()
                         .getScreenSize();
                 frame.setLocation(screenSize.width / 4, screenSize.height / 4);
                 frame.setSize(screenSize.width / 2, screenSize.height / 2);
-                JEditorPane editorPane = new JEditorPane(urls[0]);
+                JEditorPane editorPane = new JEditorPane("text/html",
+                        filledInMadlibs);
 
                 // editorPane.setText(filledInMadlibs);
-                editorPane.setSize(375, 375);
-                frame.add(editorPane);
+                frame.add(new JScrollPane(editorPane));
 
                 // frame.pack();
                 frame.setVisible(true);
