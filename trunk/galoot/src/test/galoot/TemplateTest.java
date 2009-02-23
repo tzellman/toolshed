ad51e3c163765c74f7bbe6b2290b07406dcdd7f4
tzellman <tzellman@3730442f-6e40-0410-abc2-2be2a04f140f>
2009-02-23 19:56:33 +0000

    added copyright banners

--- trunk/galoot/src/test/galoot/TemplateTest.java
+++ trunk/galoot/src/test/galoot/TemplateTest.java
@@ -1,3 +1,22 @@
+/* =============================================================================
+ * This file is part of Galoot
+ * =============================================================================
+ * (C) Copyright 2009, Tom Zellman, tzellman@gmail.com
+ *
+ * Galoot is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package galoot;
 
 import java.io.File;
@@ -638,6 +657,11 @@ public class TemplateTest extends TestCase
 
             t = new Template(
                     "{% if number <= 42 %}true{% else %}false{% endif %}");
+            output = t.render(context);
+            assertEquals(output, "true");
+
+            t = new Template(
+                    "{% if name == \"Tom\" %}true{% elseif name == \"Adam\" %}false{% endif %}");
             output = t.render(context);
             assertEquals(output, "true");
 
