6fa866ac7465f315d4dfbe3c05b239c5b1734fb2
Tom Zellman <tzellman@gmail.com>
2009-08-19 22:45:13 +0000

    added better select support, and aliases

--- rover/src/java/rover/impl/sql/SQLQueryResultSet.java
+++ rover/src/java/rover/impl/sql/SQLQueryResultSet.java
@@ -1,21 +1,21 @@
-/* =============================================================================
+/*
+ * =============================================================================
  * This file is part of Rover
  * =============================================================================
  * (C) Copyright 2009, Tom Zellman, tzellman@gmail.com
- *
+ * 
  * Rover is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
+ * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public 
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
 package rover.impl.sql;
 
@@ -165,8 +165,8 @@ public class SQLQueryResultSet implements IQueryResultSet
             String alias = key.toString();
 
             String sKey = aliasMap.containsKey(alias) ? aliasMap.get(alias)
-                    .toLowerCase() : alias;
-
+                    : alias;
+            
             Deque<String> parts = new LinkedList<String>(Arrays
                     .asList(StringUtils.splitByWholeSeparator(sKey, "__")));
 
